45c226eef98e5c9b94e1eb0fa76378568f1653f5
Rezbirn <rezbirn@gmail.com>
2024-06-16 02:16:15 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INDZ_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Window1 _window1;
+        public static string SecretPassword = "qwerty";
+        public MainWindow()
+        {
+            InitializeComponent();
+            _window1 = new Window1();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            if (Password.Text == SecretPassword && !_window1.IsVisible)
+            {
+                _window1.Owner = this;
+                _window1.ShowDialog();
+            }
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace INDZ_1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            OK.IsEnabled = false;
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.SecretPassword = PasswordNew1.Password;
+            Visibility = Visibility.Hidden;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Visibility = Visibility.Hidden;
+        }
+        private void PasswordNew_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            OnPasswordTextChanged(PasswordNew1.Password, PasswordNew2.Password);
+        }
+
+        private void OnPasswordTextChanged(string passwordNew1, string passwordNew2)
+        {
+            if (!string.IsNullOrEmpty(passwordNew1) && !string.IsNullOrEmpty(passwordNew2) && passwordNew1 == passwordNew2)
+            {
+                OK.IsEnabled = true;
+                return;
+            }
+            OK.IsEnabled = false;
+        }
+
+        
+    }
+}
